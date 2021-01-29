53f34e3d328487b34bfe8e7358fe5e354b453c63
Thaddeus Brown <tibedets@gmail.com>
2021-01-29 08:59:13 -0600

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dishes: DISHES
+        };
+    }
     render() {
-        return ( 
-        
-        <div>
-            <Navbar dark color = "primary">
-                <div className = "container" >
-                    <NavbarBrand href = "/" > Ristorante Con Fusion </NavbarBrand> 
-                </div>
-            </Navbar>
-            <Menu />
-        </div>
+        return (         
+            <div>
+                <Navbar dark color = "primary">
+                    <div className = "container" >
+                        <NavbarBrand href = "/" > Ristorante Con Fusion </NavbarBrand> 
+                    </div>
+                </Navbar>
+                <Menu dishes={this.state.dishes} />
+            </div>
         );
     }
 }
