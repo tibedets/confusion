ed3f90838fc96aac446b02cc2a18c1dee0a9eab0
Thaddeus Brown <tibedets@gmail.com>
2021-02-08 13:34:13 -0600

    Assignment 3 Solution

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,109 @@
-import React from 'react';
-import { Media, Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import {  Label, Col, Row, Button, Modal, ModalHeader, ModalBody, Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+
+    class CommentForm extends Component {
+        constructor(props) {
+            super(props);
+    
+            this.toggleModal = this.toggleModal.bind(this);
+            this.handleSubmit = this.handleSubmit.bind(this);
+    
+            this.state = {
+                isModalOpen: false
+            };
+           
+        };
+
+        toggleModal() {
+            this.setState({
+                isModalOpen: !this.state.isModalOpen
+            });
+        }
+    
+        handleSubmit(values) {
+            console.log(values.dishId, values.rating, values.author, values.comment);
+            alert('Current State is: ' + JSON.stringify(values));
+        }
+    
+        render() {
+            return (
+                <div>
+                    <Button outline onClick={this.toggleModal}>
+                        <span className="fa fa-pencil fa-lg"></span> Submit Comment
+                                        </Button>
+                    <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                        <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                        <ModalBody>
+                            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                                <Row className="form-group">
+                                    <Col md={12}>
+                                        <Label htmlFor="rating">Rating</Label>
+                                        <Control.select
+                                            model=".rating"
+                                            name="rating"
+                                            className="form-control">
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </Control.select>
+                                    </Col>
+                                </Row>
+                                <Row className="form-group">
+                                    <Col md={12}>
+                                        <Label htmlFor="author">Your Name</Label>
+                                        <Control.text
+                                            model=".author"
+                                            name="author"
+                                            className="form-control"
+                                            validators={{
+                                                required, minLength: minLength(3), maxLength: maxLength(15)
+                                            }}
+                                        />
+                                        <Errors
+                                            className="text-danger"
+                                            model=".author"
+                                            show="touched"
+                                            messages={{
+                                                required: 'Required ',
+                                                minLength: 'Must be greater than 2 characters',
+                                                maxLength: 'Must be 15 characters or less'
+                                            }}
+                                        />
+                                    </Col>
+                                </Row>
+                                <Row className="form-group">
+                                    <Col md={12}>
+                                        <Label htmlFor="comment">Comment</Label>
+                                        <Control.textarea
+                                            model=".comment"
+                                            id="comment"
+                                            name="comment"
+                                            rows="6"
+                                            className="form-control" />
+                                    </Col>
+                                </Row>
+                                <Row className="form-group">
+                                    <Col md={12}>
+                                        <Button type="submit" color="primary">
+                                            Submit
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            </LocalForm>
+                        </ModalBody>
+                    </Modal>
+                </div>
+            )
+        }
+    };
 
     function RenderDish({dish}){
         return(
@@ -16,7 +119,7 @@ import { Link } from 'react-router-dom';
         )        
     }
 
-    function RenderComments({comments}){
+    function RenderComments({comments, addComment, dishId }){
         
         const showComments = comments.map((comment)=>{
             let date = new Intl.DateTimeFormat('en-US', {
@@ -27,11 +130,9 @@ import { Link } from 'react-router-dom';
             return(
                 <div key={comment.id}>
                     <div>{comment.comment}</div>
-                    <br />
                     <div>
                         {'-- ' + comment.author + ', ' + date}
                     </div>
-                    <br />
                     <br />
                 </div>                
             );
@@ -42,6 +143,7 @@ import { Link } from 'react-router-dom';
                 <ul className="list-unstyled">
                     {showComments}
                 </ul>
+                <CommentForm dishId={dishId} addComment={addComment} />
             </div>
         );     
     }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap-social/bootstrap-social.css';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render( < App / > , document.getElementById('root'));
