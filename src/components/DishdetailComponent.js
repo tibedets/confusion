3c88d568a1465f8e2c0d38490c221c4a8bb7c623
Thaddeus Brown <tibedets@gmail.com>
2021-11-08 15:45:01 -0600

    Assignment 4 completed

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import {  Label, Col, Row, Button, Modal, ModalHeader, ModalBody, Card, CardImg,
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -109,7 +110,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
         return(
             <div className='col-12 col-md-5 m-1'>
                 <Card>
-                    <CardImg width="100%" src ={dish.image} alt ={dish.name} /> 
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} /> 
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
